08459fc918e7e64de10dc4ce94a9721130161dd7
jbartle1@nd.edu <jbartle1@nd.edu@a2479c94-90ee-a17f-40ba-06242938df42>
2012-03-21 19:28:48 +0000

    main program structure

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp - Driver for the MyPaint class
+ * CSE 20212 - Spring 2009
+ * 
+ * Author: Phil MacCart 
+ * 03-01-2009
+ * 
+ */
+ 
+ 
+#include <QApplication>
+
+#include "Draw.h"
+
+int main(int argc, char *argv[]) {
+     QApplication app(argc, argv);
+     MyPaint p;
+     p.show();
+     return app.exec();
+ }
+
