2a788e12247d0e5e705a229d09d66ffdfb50e478
jbartle1@nd.edu <jbartle1@nd.edu@a2479c94-90ee-a17f-40ba-06242938df42>
2012-03-22 00:23:02 +0000

    separating implementation from header; adding character header file

---
+++ trunk/character.h
@@ -0,0 +1,44 @@
+/*
+ *  character.cpp
+ *  Final_Project
+ *
+ *  Created by Jake Flynt on 3/4/12.
+ *  Copyright 2012. All rights reserved.
+ *
+ */
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+
+using namespace std;
+
+class character
+{
+public:
+  int getX();
+  int getY();
+
+  void setX(int n);
+  void setY(int n);
+/*
+  character(); // constructor
+  void moveLeft(); // function to move left
+  void moveRight(); // function to move right
+  void jump(); // function to jump
+  void shoot(); // function to use weapon
+*/
+private:
+  int xPos;
+  int yPos;
+/*
+  int health; // percentage of health remaining
+  int lives; // number of lives remaining
+  char press; // gets keyboard input
+*/
+};
+
+#endif
