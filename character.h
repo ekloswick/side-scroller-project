e4a9d7fa564de6f5a9471a245705ef5c6f23341e
jbartle1@nd.edu <jbartle1@nd.edu@a2479c94-90ee-a17f-40ba-06242938df42>
2012-03-24 19:14:41 +0000

    updating the display to take the number of lives from the character

--- character.h
+++ character.h
@@ -5,41 +5,40 @@
  *  Created by Jake Flynt on 3/4/12.
  *  Copyright 2012. All rights reserved.
  *
- */
-
+ */  
+  
 #ifndef CHARACTER_H
 #define CHARACTER_H
-
+  
 #include <iostream>
 #include <iomanip>
 #include <stdlib.h>
-
-using namespace std;
-
-class character
+  using namespace std;
+class character 
 {
-public:
-  character();
-  int getX();
-  int getY();
-
-  void setX(int n);
-  void setY(int n);
-
-  void moveLeft(); // function to move left
-  void moveRight(); // function to move right
-  void jump(); // function to jump
+public:
+  character ();
+  int getX ();
+  int getY ();
+  void setX (int n);
+  void setY (int n);
+  void moveLeft ();		// function to move left
+  void moveRight ();		// function to move right
+  void jump ();			// function to jump
+  int getLives ();		//gets the number of lives of the character
+  void setLives (int);		//sets the number of lives of the character
 //  void jump(); // function to jump
 //  void shoot(); // function to use weapon
-
-private:
-  int xPos;
-  int yPos;
+private:int xPos;
+  int yPos;
+  int lives;			// number of lives remaining
+  
 /*
   int health; // percentage of health remaining
-  int lives; // number of lives remaining
+
   char press; // gets keyboard input
-*/
+*/ 
 };
 
-#endif
+
+#endif	/*  */
