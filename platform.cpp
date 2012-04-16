e1cbf356993a7df524b78904af6ed919656a6072
jflynt1@nd.edu <jflynt1@nd.edu@a2479c94-90ee-a17f-40ba-06242938df42>
2012-04-16 00:22:34 +0000

    platform.cpp file upload

---
+++ platform.cpp
@@ -0,0 +1,69 @@
+/*
+Final_Project
+Justin Bartlett, Jake Flynt, Eli Kloswick
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <platform.h>
+
+#include <QPrinter>
+#include <QtGui>
+
+using namespace std;
+
+platform::platform (int X, int Y, int W, int H)
+{
+  xPos = X;
+  yPos = Y;
+  width = W;
+  height = H;
+}
+
+
+void platform::setX (int n)
+{
+  xPos = n;
+}
+
+void platform::setY (int n)
+{
+  yPos = n;
+}
+
+int platform::getX ()
+{
+  return xPos;
+}
+
+int platform::getY ()
+{
+  return yPos;
+}
+int platform::getHeight()
+{
+	return height;
+}
+
+int platform::getWidth()
+{
+	return width;
+}
+
+void platform::moveLeft(int movementSpeed)		// function to move left
+{
+  xPos -= movementSpeed;
+} 
+
+void platform::moveRight(int movementSpeed)		// function to move right
+{
+  xPos += movementSpeed;
+}
+
+
+
+
+
+
+
