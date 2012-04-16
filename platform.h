540fd677d6bf1eaf2a64ba12f707fe28ee91e214
jflynt1@nd.edu <jflynt1@nd.edu@a2479c94-90ee-a17f-40ba-06242938df42>
2012-04-16 00:23:15 +0000

    platform.h upload

---
+++ platform.h
@@ -0,0 +1,37 @@
+/*
+Final_Project
+Justin Bartlett, Jake Flynt, Eli Kloswick
+ */  
+  
+#ifndef PLATFORM_H
+#define PLATFORM_H
+  
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+  
+using namespace std;
+
+
+class platform 
+{
+	public:
+		platform(int, int, int, int);
+		void setX(int);
+		void setY(int);
+		int getX();
+		int getY();
+		int getHeight();
+		int getWidth();
+		void moveLeft(int);		// function to move left
+		void moveRight(int);	// function to move right
+
+	private:
+		int xPos; 
+		int yPos; 
+		int width;
+		int height;
+
+};
+
+#endif
