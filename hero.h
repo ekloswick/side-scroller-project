29e156705cf675bc8fc69602c70fb704f2fea6d2
jbartle1@nd.edu <jbartle1@nd.edu@a2479c94-90ee-a17f-40ba-06242938df42>
2012-04-24 03:51:37 +0000

    adding hero.h

---
+++ hero.h
@@ -0,0 +1,57 @@
+/*
+ *  character.cpp
+ *  Final_Project
+ *
+ *  Created by Jake Flynt on 3/4/12.
+ *  Copyright 2012. All rights reserved.
+ *
+ */  
+  
+#ifndef HERO_H
+#define HERO_H
+  
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include "character.h"
+
+
+// *** CHECK TO MAKE SURE CHARACTER IS ALL GONE
+  
+using namespace std;
+
+class hero : public character
+{
+
+    public:
+          hero (int, int, int);
+      
+        int getXVel ();
+        int getYVel ();
+        int getXSize();
+        int getYSize();
+
+        void setXSize(int);
+        void setYSize(int);
+        
+        int isJumping;
+
+        void setXVel (int);
+        void setYVel (int);
+        void moveLeft ();        // function to move left
+        void moveRight ();        // function to move right
+
+        int getGravity ();
+        void jump ();            // function to jump
+        
+    private:
+        int xVel;            // acceleration in x direction
+          int yVel;            // acceleration in y direction
+         int gravity;
+        int xsize;
+        int ysize;
+};
+
+
+
+#endif
