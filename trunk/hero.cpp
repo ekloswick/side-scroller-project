7b36445f980ac3b68d5797a70fe7b43c42cc5acb
jbartle1@nd.edu <jbartle1@nd.edu@a2479c94-90ee-a17f-40ba-06242938df42>
2012-04-24 03:50:30 +0000

    adding hero.cpp class from inheritance mentioned in previous revision

---
+++ trunk/hero.cpp
@@ -0,0 +1,93 @@
+/*
+ *  hero.cpp
+ *  Final_Project
+ *
+ *  Created by Jake Flynt on 3/4/12.
+ *  Copyright 2012. All rights reserved.
+ *
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <hero.h>
+#include <QPrinter>
+#include <QtGui>
+
+using namespace std;
+
+hero::hero(int x, int y, int lives) : character (x,y,lives)
+{
+    setXVel(0);
+    setYVel(getGravity());
+    gravity = 25;
+    character::rightFacing=1;
+    setXSize(98);
+    setYSize(164);
+    movementSpeed=16;
+}
+
+
+void hero::moveLeft ()    // function to move left
+{
+xVel-=movementSpeed;
+}
+void hero::moveRight ()        // function to move right
+{
+xVel+=movementSpeed;
+}
+
+void hero::jump()
+{
+    yVel = -80;
+}
+
+
+int hero::getXVel()
+{
+    return xVel;
+}
+
+int hero::getYVel()
+{
+    return yVel;
+}
+
+void hero::setXVel(int n)
+{
+    xVel = n;
+}
+
+void hero::setYVel(int n)
+{
+    yVel = n;
+}
+
+int hero::getGravity()
+{
+    return gravity;
+}
+
+int hero::getXSize()
+{
+    return xsize;
+}
+
+int hero::getYSize()
+{
+    return ysize;
+}
+
+void hero::setXSize(int n)
+{
+    xsize = n;
+}
+
+void hero::setYSize(int n)
+{
+    ysize = n;
+}
+
+
+
+
