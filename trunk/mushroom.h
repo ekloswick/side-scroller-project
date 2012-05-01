760f0daeba7f1cf3f52265df1a1ded49f170225f
jbartle1@nd.edu <jbartle1@nd.edu@a2479c94-90ee-a17f-40ba-06242938df42>
2012-05-01 02:10:51 +0000

    added mushroom class and header file

---
+++ trunk/mushroom.h
@@ -0,0 +1,40 @@
+/*
+CSE 20212
+Final Project: Super Mario Side Scroller 
+
+Created by Jake Flynt, Justin Bartlett, Eli Kloswick.
+Copyright 2012. All rights reserved.
+
+"mushroom.h"
+ */ 
+  
+#ifndef MUSHROOM_H
+#define MUSHROOM_H
+  
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include "character.h"
+  using namespace std;
+class mushroom
+{
+public:mushroom (int, int);
+
+	void moveWithPlatform (int);
+	void setXPos(int);
+	void setYPos(int);
+	int getXPos(void);
+	int getYPos(void);
+
+	int getXSize(void);
+	int getYSize(void);
+
+void update(int, int);
+private:
+	int xPos;
+	int yPos;
+	int xSize;
+	int ySize;
+};
+
+#endif
