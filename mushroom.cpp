81909b97e023a622600ee52500490be925ec12ad
jbartle1@nd.edu <jbartle1@nd.edu@a2479c94-90ee-a17f-40ba-06242938df42>
2012-05-01 02:10:51 +0000

    added mushroom class and header file

---
+++ mushroom.cpp
@@ -0,0 +1,71 @@
+/*
+CSE 20212
+Final Project: Super Mario Side Scroller 
+
+Created by Jake Flynt, Justin Bartlett, Eli Kloswick.
+Copyright 2012. All rights reserved.
+
+"mushroom.cpp"
+ */ 
+
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <mushroom.h>
+#include "character.h"
+#include <QPrinter>
+#include <QtGui>
+
+using namespace std;
+
+mushroom::mushroom (int x, int y)
+{
+  xPos=x;
+  yPos=y; 
+  xSize = 16;
+  ySize = 16;
+}
+
+void mushroom::setXPos(int x)
+{
+xPos=x;
+}
+
+void mushroom::setYPos(int y)
+{
+yPos=y;
+}
+
+int mushroom::getXPos(void)
+{
+return xPos;
+}
+
+int mushroom::getYPos(void)
+{
+return yPos;
+}
+
+int mushroom::getXSize(void)
+{
+return xSize;
+}
+
+int mushroom::getYSize(void)
+{
+return ySize;
+}
+
+
+void
+mushroom::moveWithPlatform (int speed)
+{
+  setXPos (getXPos () - speed);
+}
+
+void 
+mushroom::update(int x, int y)
+{
+xPos=x;
+yPos=y;
+}
